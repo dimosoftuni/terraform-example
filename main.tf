ffae27a2c4e034e136835b70fb537a9cc4ac95e4
Dimo Mitev <104933623+dimosoftuni@users.noreply.github.com>
2024-02-06 20:31:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.89.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstorage"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
